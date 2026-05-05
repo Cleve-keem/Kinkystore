e34fd54d550fba83e9041f936444fff9c1078987
Cleve-keem <belloharyourmidey@gmail.com>
2026-05-05 06:45:28 -0500

    feat(product): added product card UI component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
 import Header from "@/components/Header";
 import Navbar3 from "@/components/Navbar3";
+import ProductCard from "@/components/products/productCard";
 import { productService } from "@/services/products";
-import Image from "next/image";
 
 export default async function Home() {
   const allProducts = await productService.getAllProducts();
-  console.log(allProducts);
 
   return (
     <>
       <Header />
       <Navbar3 />
-      <main>
-        {allProducts.map((product: any) => (
-          <div key={product.id}>
-            <Image
-              src={product.imageUrl}
-              alt={product.name}
-              width={200}
-              height={200}
-              priority={product.id < 4}
-            />
-            <p>{product.name}</p>
-          </div>
+      <main className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+        {allProducts.map((product: any, index: number) => (
+          <ProductCard product={product} index={index} key={product.id} />
         ))}
       </main>
     </>
--- services/products.ts
+++ services/products.ts
@@ -1,20 +1,46 @@
 import { prisma } from "@/libs/prisma";
+import { unstable_cache } from "next/cache";
 
 class ProductServices {
-  async getAllProducts() {
-    try {
-      const allProducts = await prisma.product.findMany({
-        orderBy: {
-          createdAt: "desc",
-        },
-      });
-
-      return allProducts;
-    } catch (error: any) {
-      console.error("Error fetching products:", error);
-      return [];
-    }
-  }
+  getAllProducts = unstable_cache(
+    async () => {
+      try {
+        console.log("disturbing the database...");
+        return await prisma.product.findMany({
+          orderBy: { createdAt: "desc" },
+        });
+      } catch (error: any) {
+        console.error("Error fetching products:", error);
+        return [];
+      }
+    },
+    ["all-products-key"],
+    {
+      revalidate: 3600,
+      tags: ["products"],
+    },
+  );
 }
 
 export const productService = new ProductServices();
+
+// import { prisma } from "@/libs/prisma";
+
+// class ProductServices {
+//   async getAllProducts() {
+//     try {
+//       const allProducts = await prisma.product.findMany({
+//         orderBy: {
+//           createdAt: "desc",
+//         },
+//       });
+
+//       return allProducts;
+//     } catch (error: any) {
+//       console.error("Error fetching products:", error);
+//       return [];
+//     }
+//   }
+// }
+
+// export const productService = new ProductServices();
